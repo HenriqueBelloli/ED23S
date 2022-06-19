28525bd8e829bc7400010ab0062fd85bbcf0b26d
HenriqueBelloli <belloli.henrique@gmail.com>
2022-06-19 20:41:41 -0300

    Finalizando

--- Pratica_05/at4.c
+++ Pratica_05/at4.c
@@ -7,47 +7,51 @@
 #include <string.h>
 #include <ctype.h>
 
+// -printDados------------------------------
+void printDados(FILE *arq, char *metodo, int *array, int tamanho, int comparacoes, clock_t tempo)
+{
 
-// -------------------------------
-void printDados(FILE *arq, char* metodo, int *array, int tamanho, int comparacoes, clock_t tempo){
-    
     fprintf(arq, "%s:", metodo);
 
-    for (int i = 0; i < tamanho; i++)    {
+    for (int i = 0; i < tamanho; i++)
+    {
         fprintf(arq, " %d", array[i]);
     }
 
     fprintf(arq, ", %d comp, %d ms\n", comparacoes, tempo);
-
 }
 
-// -------------------------------
-void insertionSort(int tamanho, int *array, FILE *arquivo){
-    
+// -insertionSort-----------------------------
+void insertionSort(int tamanho, int *array, FILE *arquivo)
+{
+
     int i, j, aux, comparacoes = 0;
     clock_t start, end, diff;
     start = clock();
 
-    for(i = 1; i < tamanho; i++){
+    for (i = 1; i < tamanho; i++)
+    {
         aux = array[i];
-        j = i-1;
-        
-        while (j >= 0 && (aux < array[j]) ) {
-            array[j+1] = array[j];
+        j = i - 1;
+
+        while (j >= 0 && (aux < array[j]))
+        {
+            array[j + 1] = array[j];
             j--;
             comparacoes++;
         }
         comparacoes++;
-        array[j+1] = aux;
+        array[j + 1] = aux;
     }
 
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
     printDados(arquivo, "insertionSort", array, tamanho, comparacoes, diff);
 }
 
-// -------------------------------
-void selectionSort(int tamanho, int *array, FILE *arquivo){
+// -selectionSort------------------------------
+void selectionSort(int tamanho, int *array, FILE *arquivo)
+{
     int comparacoes = 0;
     int menor, aux;
     clock_t start, end, diff;
@@ -73,17 +77,17 @@ void selectionSort(int tamanho, int *array, FILE *arquivo){
             array[menor] = array[N];
             array[N] = aux;
         }
-        
     }
 
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
 
     printDados(arquivo, "selectionSort", array, tamanho, comparacoes, diff);
 }
 
-// -------------------------------
-void bubbleSort(int tamanho, int *array, FILE *arquivo){
+// -bubbleSort------------------------------
+void bubbleSort(int tamanho, int *array, FILE *arquivo)
+{
 
     int comparacoes = 0;
     bool trocou = true;
@@ -107,144 +111,214 @@ void bubbleSort(int tamanho, int *array, FILE *arquivo){
         }
     }
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
 
     printDados(arquivo, "bubbleSort", array, tamanho, comparacoes, diff);
 }
 
-// -------------------------------
-void merge(int *array, int inicio, int meio, int fim, int *comparacoes) {
+// -mergeSort------------------------------
+void merge(int *array, int inicio, int meio, int fim, int *comparacoes)
+{
 
     // Alocar dinamicamente um vetor auxiliar só para os elementos da recursão
-        int tamArray = (fim - inicio)+1;
-        int *aux = malloc(tamArray * sizeof(int));
+    int tamArray = (fim - inicio) + 1;
+    int *aux = malloc(tamArray * sizeof(int));
 
-        int p1 = inicio;
-        int p2 = meio + 1;
-        int contAux = 0;
+    int p1 = inicio;
+    int p2 = meio + 1;
+    int contAux = 0;
 
     // copia para o vetor auxiliar o menor valor entre array[p1] e array[p2]
-        while(p1 <= meio && p2 <= fim) {
-            (*comparacoes) = (*comparacoes) + 1;
-            if(array[p1] < array[p2]) {
-                aux[contAux++] = array[p1++];
-            } else {
-                aux[contAux++] = array[p2++];
-            }
+    while (p1 <= meio && p2 <= fim)
+    {
+        (*comparacoes) = (*comparacoes) + 1;
+        if (array[p1] < array[p2])
+        {
+            aux[contAux++] = array[p1++];
         }
+        else
+        {
+            aux[contAux++] = array[p2++];
+        }
+    }
 
     // Se P1 > meio, sobraram elementos em P2
-        int contIni, contFim;
-        if(p1 > meio) {
-            contFim = fim - p2 + 1;
-            for(contIni = 0; contIni < contFim; contIni++) {
-                aux[contAux++] = array[p2++];
-            }
-        } else { // Senão, P2 > Fim, sobraram elementos em P1
-            contFim = meio - p1 + 1;
-            for(contIni = 0; contIni < contFim; contIni++) {
-                aux[contAux++] = array[p1++];
-            }
+    int contIni, contFim;
+    if (p1 > meio)
+    {
+        contFim = fim - p2 + 1;
+        for (contIni = 0; contIni < contFim; contIni++)
+        {
+            aux[contAux++] = array[p2++];
         }
+    }
+    else
+    { // Senão, P2 > Fim, sobraram elementos em P1
+        contFim = meio - p1 + 1;
+        for (contIni = 0; contIni < contFim; contIni++)
+        {
+            aux[contAux++] = array[p1++];
+        }
+    }
 
     // copia elementos do aux para array
-        contIni = 0;
-        contAux = inicio;
-        while(contIni < tamArray) {
-            array[contAux] = aux[contIni];
-            contIni++; contAux++;
-        }
-        free(aux);
+    contIni = 0;
+    contAux = inicio;
+    while (contIni < tamArray)
+    {
+        array[contAux] = aux[contIni];
+        contIni++;
+        contAux++;
+    }
+    free(aux);
 }
 
-void mergeSort(int *array, int inicio, int fim, int *comparacoes){
-    
-    if (inicio < fim){
-        int meio = (int)floor((inicio + fim)/2);
+void mergeSort(int *array, int inicio, int fim, int *comparacoes)
+{
+
+    if (inicio < fim)
+    {
+        int meio = (int)floor((inicio + fim) / 2);
         mergeSort(array, inicio, meio, comparacoes);
-        mergeSort(array, meio+1, fim, comparacoes);
+        mergeSort(array, meio + 1, fim, comparacoes);
         merge(array, inicio, meio, fim, comparacoes);
     }
 }
 
-void mergeSortPrincipal(int tamanho, int *array, FILE *arquivo){
+void mergeSortPrincipal(int tamanho, int *array, FILE *arquivo)
+{
     int comparacoes = 0;
     clock_t start, end, diff;
     start = clock();
-    
+
     mergeSort(array, 0, tamanho - 1, &comparacoes);
 
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
 
     printDados(arquivo, "mergeSort", array, tamanho, comparacoes, diff);
 }
 
-// -------------------------------
-int particiona(int *vetor, int inicio, int fim, int *comparacoes) {
+// -quickSort------------------------------
+void quickSort(int *array, int inicio, int fim, int *comparacoes)
+{
+    int esquerda, direita, pivo, aux;
 
-    int esquerda = inicio;
-    int direita  = fim;
-    int pivo     = vetor[inicio];
+    if (inicio < fim)
+    {
+        pivo = inicio;
+        esquerda = inicio;
+        direita = fim;
 
-    while(esquerda < direita) {
-        while(vetor[esquerda] <= vetor[pivo]) {
-            esquerda++;
+        while (esquerda < direita)
+        {
+            while (array[esquerda] <= array[pivo] && esquerda < fim)
+                esquerda++;
+            while (array[direita] > array[pivo])
+                direita--;
+            if (esquerda < direita)
+            {
+                aux = array[esquerda];
+                array[esquerda] = array[direita];
+                array[direita] = aux;
+            }
+            *comparacoes = *comparacoes + 1;
         }
-        while(vetor[direita] > pivo) {
-            direita--;
-        }
-        if(esquerda < direita) {
-            //(*comparacoes) = (*comparacoes) + 1;
-            int aux = vetor[esquerda];
-            vetor[esquerda] = vetor[direita];
-            vetor[direita] = aux;
-        }
-    }
 
-  vetor[inicio]  = vetor[direita];
-  vetor[direita] = pivo;
-
-  return(direita);
-}
-
-void quickSort(int *vetor, int inicio, int fim, int *comparacoes) {
-    int pivo;
-    if(fim > inicio ) {
-        pivo = particiona(vetor, inicio, fim, comparacoes);
-        quickSort(vetor, inicio, pivo-1, comparacoes);
-        quickSort(vetor, pivo+1, fim, comparacoes);
+        aux = array[pivo];
+        array[pivo] = array[direita];
+        array[direita] = aux;
+        quickSort(array, inicio, direita - 1, comparacoes);
+        quickSort(array, direita + 1, fim, comparacoes);
     }
 }
-
-void quickSortPrincipal(int tamanho, int *array, FILE *arquivo){
+void quickSortPrincipal(int tamanho, int *vetor, FILE *arquivo)
+{
     int comparacoes = 0;
     clock_t start, end, diff;
     start = clock();
 
-    quickSort(array, 0, tamanho - 1, &comparacoes);
+    quickSort(vetor, 0, tamanho - 1, &comparacoes);
 
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
 
-    printDados(arquivo, "quickSort", array, tamanho, comparacoes, diff);
+    printDados(arquivo, "quickSort", vetor, tamanho, comparacoes, diff);
 }
 
-// -------------------------------
-void heapSort(int tamanho, int *array, FILE *arquivo){
+// -heapSort------------------------------
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int *array, int tamanho, int posicaoMaior, int *comparacoes)
+{
+    int maior = posicaoMaior;
+    int esquerda = 2 * posicaoMaior + 1;
+    int direita = 2 * posicaoMaior + 2;
+    *comparacoes = *comparacoes + 1;
+
+    if (esquerda < tamanho && array[esquerda] > array[maior])
+    {
+        maior = esquerda;
+    }
+
+    if (direita < tamanho && array[direita] > array[maior])
+    {
+        maior = direita;
+    }
+
+    if (maior != posicaoMaior)
+    {
+        swap(&array[posicaoMaior], &array[maior]);
+        heapify(array, tamanho, maior, comparacoes);
+    }
+}
+
+void heapSort(int tamanho, int *array, FILE *arquivo)
+{
+
+    int comparacoes = 0;
+    clock_t start, end, diff;
+    start = clock();
+
+    for (int i = tamanho / 2 - 1; i >= 0; i--)
+    {
+        heapify(array, tamanho, i, &comparacoes);
+    }
+
+    for (int i = tamanho - 1; i >= 0; i--)
+    {
+        swap(&array[0], &array[i]);
+        heapify(array, i, 0, &comparacoes);
+    }
+
+    end = clock();
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
+
+    printDados(arquivo, "heapSort", array, tamanho, comparacoes, diff);
+}
+
+void adadaheapSort(int tamanho, int *array, FILE *arquivo)
+{
     int comparacoes = 0;
     clock_t start, end, diff;
     start = clock();
     end = clock();
-    diff = (end - start)/(CLOCKS_PER_SEC/1000);
+    diff = (end - start) / (CLOCKS_PER_SEC / 1000);
 
     printDados(arquivo, "heapSort", array, tamanho, comparacoes, diff);
 }
 
 // -------------------------------
-void copiaMatriz(int tamanho, int *origem, int *destino){
-    
-    for (int i = 0; i < tamanho; i++)    {
+void copiaMatriz(int tamanho, int *origem, int *destino)
+{
+
+    for (int i = 0; i < tamanho; i++)
+    {
         destino[i] = origem[i];
     }
 }
@@ -252,90 +326,144 @@ void copiaMatriz(int tamanho, int *origem, int *destino){
 int main(int argc, const char *argv[])
 {
 
-     // Valida se recebeu os parâmetros necessários.
-        if (argc != 3)
-        {
-            printf("Erro: Quantidade de parametros invalida\n");
-            //return(1);
-        }
+    // Valida se recebeu os parâmetros necessários.
+    if (argc != 3)
+    {
+        printf("Erro: Quantidade de parametros invalida\n");
+        return(1);
+    }
 
     // Abre os arquivos.
-        //FILE *arquivoLeitura = fopen(argv[1], "r");
-        //FILE *arquivoEscrita = fopen(argv[2], "w");
-        FILE *arquivoLeitura = fopen("C:\\temp\\in.txt", "r");
-        FILE *arquivoEscrita = fopen("C:\\temp\\saida.txt", "w");
+        FILE *arquivoLeitura = fopen(argv[1], "r");
+        FILE *arquivoEscrita = fopen(argv[2], "w");
+        //FILE *arquivoLeitura = fopen("C:\\temp\\input3.txt", "r");
+        //FILE *arquivoEscrita = fopen("C:\\temp\\saida.txt", "w");
 
-        if (arquivoLeitura == NULL)
-        {
-            printf("Erro: Falha ao abrir o arquivo para leitura!\n");
-            //return(1);
+    if (arquivoLeitura == NULL)
+    {
+        printf("Erro: Falha ao abrir o arquivo para leitura!\n");
+        return(1);
+    }
+
+    if (arquivoEscrita == NULL)
+    {
+        printf("Erro: Falha ao abrir arquivo para escrita!\n");
+        return (1);
+    }
+    printf("Log: Arquivos abertos.\n");
+
+    // le o conteudo
+    bool arquivoValido = true;
+    char formato, linha[300];
+    int tamanho, cont, contlinhas = 0;
+
+    while (!feof(arquivoLeitura) && contlinhas < 2)
+    {
+        fscanf(arquivoLeitura, " %[^\n]s", linha);
+        contlinhas++;
+        printf("Log: Linha %d= %s \n", contlinhas, linha);
+        
+        if (strlen(linha) == 0 || linha[0] == '\0'){
+            printf("Erro: Linha sem conteudo.\n");
+            arquivoValido = false;
+            break;
         }
 
-        if (arquivoEscrita == NULL)
-        {
-            printf("Erro: Falha ao abrir arquivo para escrita!\n");
-            return(1);
-        }
-        printf("Log: Arquivos abertos.\n");
-    
-    // Verifica a entrada
-        bool arquivoValido = true;
-        int tamanho= 10;
-        char formato = 'd';
-    
-    if(arquivoValido){
-        // Monta o vetor
-            int contador, valor, vetor[tamanho], vetorAUX[tamanho];
-            srand(time(NULL));
-
-            if(formato == 'c'){
-                valor = 0;
-            }else if(formato == 'd'){
-                valor= tamanho+1;
-            }
-
-            for (contador = 0; contador < tamanho; contador++){
-            
-                if(formato == 'c'){
-                    valor++;
-                }else if(formato == 'd'){
-                    valor--;
-                }else{
-                    valor = (rand() % 32000);
+        if (contlinhas == 1){
+            // Verifica se possui apenas números
+            for (cont = 0; cont < strlen(linha); cont ++){
+                if (!isdigit(linha[cont])){
+                    arquivoValido = false;
+                    break;
                 }
-
-                vetor[contador] = valor;
+            }
+            if (!arquivoValido){
+                printf("Erro: Linha de tamanho do vetor invalida\n");
+                break;
             }
 
-        //Executa as ordenacoes
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            insertionSort(tamanho, vetorAUX, arquivoEscrita);
-            
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            selectionSort(tamanho, vetorAUX, arquivoEscrita);
-            
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            bubbleSort(tamanho, vetorAUX, arquivoEscrita);
-            
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            mergeSortPrincipal(tamanho, vetorAUX, arquivoEscrita);
-            
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            quickSortPrincipal(tamanho, vetorAUX, arquivoEscrita);
+            tamanho = atoi(linha);
+        }else{
+            if (strlen(linha) == 1 && (linha[0] == 'c' || linha[0] == 'd' || linha[0] == 'r')){
+                formato = linha[0];
+            }else{
+                printf("Erro: Modo de geração do vetor invalido\n");
+                arquivoValido = false;
+                break;
+            }
+        }
+    }
 
-            copiaMatriz(tamanho, vetor, vetorAUX);
-            heapSort(tamanho, vetorAUX, arquivoEscrita);
+    if(! contlinhas > 0){
+        arquivoValido = false;
+    }
+
+    if (arquivoValido)
+    {
+        // Monta o vetor
+        int contador, valor, vetor[tamanho], vetorAUX[tamanho];
+        srand(time(NULL));
+
+        if (formato == 'c')
+        {
+            valor = 0;
+        }
+        else if (formato == 'd')
+        {
+            valor = tamanho + 1;
+        }
+
+        for (contador = 0; contador < tamanho; contador++)
+        {
+
+            if (formato == 'c')
+            {
+                valor++;
+            }
+            else if (formato == 'd')
+            {
+                valor--;
+            }
+            else
+            {
+                valor = (rand() % 32000);
+            }
+
+            vetor[contador] = valor;
+        }
+
+        // Executa as ordenacoes
+        printf("Log: Executando... insertionSort");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        insertionSort(tamanho, vetorAUX, arquivoEscrita);
+
+        printf("\\ selectionSort");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        selectionSort(tamanho, vetorAUX, arquivoEscrita);
+
+        printf("\\ bubbleSort");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        bubbleSort(tamanho, vetorAUX, arquivoEscrita);
+
+        printf("\\ mergeSort");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        mergeSortPrincipal(tamanho, vetorAUX, arquivoEscrita);
+
+        printf("\\ quickSort");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        quickSortPrincipal(tamanho, vetorAUX, arquivoEscrita);
+
+        printf("\\ heapSort.\n");
+        copiaMatriz(tamanho, vetor, vetorAUX);
+        heapSort(tamanho, vetorAUX, arquivoEscrita);
     }else{
-        fprintf(arquivoEscrita, "%s", "Arquivo de entrada inválido!");
+        fprintf(arquivoEscrita, "%s", "Arquivo de entrada invalido!");
     }
 
     // Fecha os arquivos
-        fclose(arquivoLeitura);
-        fclose(arquivoEscrita);
-        printf("\nLog: Arquivos fechados!\n");
+    fclose(arquivoLeitura);
+    fclose(arquivoEscrita);
+    printf("Log: Arquivos fechados!\n");
 
     return 0;
 }
-
-// -------------------------------
-// -------------------------------
